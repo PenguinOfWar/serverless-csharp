d002acb14b4094c400fb9447e74b29709c6eaafc
Darryl Walker <darryljwalker@gmail.com>
2020-11-26 16:27:38 +0000

    Commit sln for intellisense detection

--- Handler.cs
+++ Handler.cs
@@ -11,76 +11,76 @@ typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AwsDotnetCsharp
 {
-    public class Handler
+  public class Handler
+  {
+
+    public APIGatewayProxyResponse HelloWorld(APIGatewayProxyRequest request, ILambdaContext context)
     {
+      APIGatewayProxyResponse response;
+      Dictionary<string, string> dict = new Dictionary<string, string>();
+      dict.Add("hello", "world");
+      response = CreateResponse(dict);
+      return response;
+    }
 
-        public APIGatewayProxyResponse HelloWorld(APIGatewayProxyRequest request, ILambdaContext context)
+    public APIGatewayProxyResponse GetQuerystring(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+      APIGatewayProxyResponse response;
+      LogMessage(context, "Processing request started");
+      if (request != null && request.QueryStringParameters.Count > 0)
+      {
+        try
         {
-            APIGatewayProxyResponse response;
-            Dictionary<string, string> dict = new Dictionary<string, string>();
-            dict.Add("hello", "world");
-            response = CreateResponse(dict);
-            return response;
+          // var result = processor.CurrentTimeUTC();
+          response = CreateResponse(request.QueryStringParameters);
+          LogMessage(context, "First Parameter Value to read is: " + request.QueryStringParameters["foo"]);
+          LogMessage(context, "Processing request succeeded.");
         }
-
-        public APIGatewayProxyResponse GetQuerystring(APIGatewayProxyRequest request, ILambdaContext context)
+        catch (Exception ex)
         {
-            APIGatewayProxyResponse response;
-            LogMessage(context, "Processing request started");
-            if (request != null && request.QueryStringParameters.Count > 0)
-            {
-                try
-                {
-                    // var result = processor.CurrentTimeUTC();
-                    response = CreateResponse(request.QueryStringParameters);
-                    LogMessage(context, "First Parameter Value to read is: " + request.QueryStringParameters["foo"]);
-                    LogMessage(context, "Processing request succeeded.");
-                }
-                catch (Exception ex)
-                {
-                    LogMessage(context, string.Format("Processing request failed - {0}", ex.Message));
-                    response = CreateResponse(null);
-                }
-            }
-            else
-            {
-                LogMessage(context, "Processing request failed - Please add queryStringParameter 'foo' to your request - see sample in readme");
-                response = CreateResponse(null);
-            }
-            return response;
+          LogMessage(context, string.Format("Processing request failed - {0}", ex.Message));
+          response = CreateResponse(null);
         }
-        void LogMessage(ILambdaContext ctx, string msg)
-        {
-            ctx.Logger.LogLine(
-                string.Format("{0}:{1} - {2}",
-                    ctx.AwsRequestId,
-                    ctx.FunctionName,
-                    msg));
-        }
-        APIGatewayProxyResponse CreateResponse(IDictionary<string, string> result)
-        {
-            int statusCode = (result != null) ?
-                (int)HttpStatusCode.OK :
-                (int)HttpStatusCode.InternalServerError;
+      }
+      else
+      {
+        LogMessage(context, "Processing request failed - Please add queryStringParameter 'foo' to your request - see sample in readme");
+        response = CreateResponse(null);
+      }
+      return response;
+    }
+    void LogMessage(ILambdaContext ctx, string msg)
+    {
+      ctx.Logger.LogLine(
+          string.Format("{0}:{1} - {2}",
+              ctx.AwsRequestId,
+              ctx.FunctionName,
+              msg));
+    }
+    APIGatewayProxyResponse CreateResponse(IDictionary<string, string> result)
+    {
+      int statusCode = (result != null) ?
+          (int)HttpStatusCode.OK :
+          (int)HttpStatusCode.InternalServerError;
 
-            string body = (result != null) ?
-                JsonConvert.SerializeObject(result) : string.Empty;
+      string body = (result != null) ?
+          JsonConvert.SerializeObject(result) : string.Empty;
 
-            var response = new APIGatewayProxyResponse
-            {
-                StatusCode = statusCode,
-                Body = body,
-                Headers = new Dictionary<string, string>
+      var response = new APIGatewayProxyResponse
+      {
+        StatusCode = statusCode,
+        Body = body,
+        Headers = new Dictionary<string, string>
                 {
                     { "Content-Type", "application/json" },
                     { "Access-Control-Allow-Origin", "*" }
                 }
-            };
+      };
 
-            return response;
-        }
-
+      return response;
     }
+
+  }
 
 
 
